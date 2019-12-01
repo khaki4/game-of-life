8c47e12439aa8f4f7591e036d5505920ec163a6b
pi3rrot <4dakto@gmail.com>
2019-12-01 13:12:31 +0900

    file 분기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React, {MutableRefObject, useCallback, useEffect, useRef} from "react";
 import {useState} from "react";
-import produce from 'immer';
+import produce from "immer";
+import GameItem from "./GameItem";
+import {itemSize, numCols, numRows, operations} from './constant';
 
-const numRows = 50;
-const numCols = 50;
-const operations = [[0, 1], [0, -1], [1, -1], [-1, 1], [1, 1], [-1, -1], [1, 0], [-1, 0]];
 const getCurrentTime = () => new Date().getTime();
 
-const gridManger = new class {
+const gridManger = new (class {
   setToOff = () => 0;
-  setToRandom = () => Math.random() > 0.5 ? 1 : 0;
+  setToRandom = () => (Math.random() > 0.5 ? 1 : 0);
   generateGrid = (setValue: () => number) => () => {
     const rows = [];
     for (let i = 0; i < numRows; i++) {
@@ -17,69 +16,104 @@ const gridManger = new class {
     }
     return rows;
   };
-}();
+})();
+
+const makeOnClickHandler = (grid: any, setGrid: any) => ([x, y]: [
+  number,
+  number
+]) => () => {
+  const newGrid = produce(grid, (gridCopy: any) => {
+    gridCopy[x][y] = grid[x][y] ? 0 : 1;
+  });
+  setGrid(newGrid);
+};
 
 const App: React.FC = React.memo(() => {
-  const [grid, setGrid] = useState(gridManger.generateGrid(gridManger.setToOff));
-  const [running, setRunning] = useState(false);
-  const runningRef: MutableRefObject<boolean> = useRef(running);
-  useEffect(() => {
-    runningRef.current = running;
-  }, [runningRef.current, running]);
-  const runSimulation = useSimulation(runningRef, setGrid);
+  const {
+    grid,
+    setGrid,
+    running,
+    onClickHandlerWidthGrid,
+    onClickToggle,
+    onClickRandom,
+    onClickClear
+  } = useGameOfLife();
 
   return (
     <>
-      <button onClick={() => {
-        setRunning(!running);
-        if (!running) {
-          runningRef.current = true;
-          runSimulation(getCurrentTime());
-        }
-      }}>{running ? 'stop' : 'start'}</button>
-      <button onClick={() => {
-        const rows = [];
-        for (let i = 0; i < numRows; i++) {
-          rows.push(Array.from(Array(numCols), gridManger.setToRandom));
-        }
-        setGrid(rows);
-      }}>
-        random
-      </button>
-      <button onClick={() => {
-        setGrid(gridManger.generateGrid(gridManger.setToOff));
-      }}>
-        clear
-      </button>
-      <div style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 20px)`
-      }}>
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <div
-              key={`${i}-${k}`}
-              onClick={() => {
-                const newGrid = produce(grid, gridCopy => {
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                })
-                setGrid(newGrid);
-              }}
-              style={{
-                width: 20,
-                height: 20,
-                backgroundColor: grid[i][k] ? 'pink' : undefined,
-                border: 'solid 1px black'
-              }}/>
-          ))
-        )}
+      <button onClick={onClickToggle}>{running ? "stop" : "start"}</button>
+      <button onClick={onClickRandom}>random</button>
+      <button onClick={onClickClear}>clear</button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, ${itemSize}px)`
+        }}
+      >
+        {grid.map((rows, i) => {
+          return rows.map((_, k) => {
+            return (
+              <GameItem
+                key={`${i}-${k}`}
+                grid={grid}
+                setGrid={setGrid}
+                i={i}
+                k={k}
+                onClickHandlerWidthGrid={onClickHandlerWidthGrid}
+              />
+            );
+          });
+        })}
       </div>
-
     </>
   );
 });
 
-function useSimulation(runningRef: MutableRefObject<boolean>, setGrid: (value: (((prevState: any[]) => any[]) | any[])) => void) {
+
+function useGameOfLife() {
+  const [grid, setGrid] = useState(
+    gridManger.generateGrid(gridManger.setToOff)
+  );
+  const [running, setRunning] = useState(false);
+  const runningRef: MutableRefObject<boolean> = useRef(running);
+  useEffect(() => {
+    runningRef.current = running;
+  }, [running]);
+  const runSimulation = useSimulation(runningRef, setGrid);
+  const onClickHandlerWidthGrid = makeOnClickHandler(grid, setGrid);
+  const onClickToggle = useCallback(() => {
+    setRunning(!running);
+    if (!running) {
+      runningRef.current = true;
+      runSimulation(getCurrentTime());
+    }
+  }, [setRunning, running, runningRef.current, runSimulation, getCurrentTime]);
+  const onClickRandom = useCallback(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), gridManger.setToRandom));
+    }
+    setGrid(rows);
+  }, [setGrid]);
+  const onClickClear = useCallback(() => {
+    setGrid(gridManger.generateGrid(gridManger.setToOff));
+  }, [setGrid]);
+
+  return {
+    grid,
+    setGrid,
+    running,
+    onClickHandlerWidthGrid,
+    onClickToggle,
+    onClickRandom,
+    onClickClear
+  };
+}
+
+function useSimulation(
+  runningRef: MutableRefObject<boolean>,
+  setGrid: (value: ((prevState: any[]) => any[]) | any[]) => void
+) {
   const simulation = useCallback(function recur(startTime) {
     let _startTime = startTime;
     const time = getCurrentTime() - _startTime;
@@ -88,7 +122,7 @@ function useSimulation(runningRef: MutableRefObject<boolean>, setGrid: (value: (
     }
 
     if (time >= 100) {
-      setGrid((grid) => {
+      setGrid(grid => {
         return produce(grid, gridCopy => {
           for (let i = 0; i < numRows; i++) {
             for (let k = 0; k < numCols; k++) {
@@ -97,7 +131,12 @@ function useSimulation(runningRef: MutableRefObject<boolean>, setGrid: (value: (
               operations.forEach(([x, y]) => {
                 const newI = i + x;
                 const newK = k + y;
-                if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                if (
+                  newI >= 0 &&
+                  newI < numRows &&
+                  newK >= 0 &&
+                  newK < numCols
+                ) {
                   neighbors += grid[newI][newK];
                 }
               });
@@ -109,7 +148,7 @@ function useSimulation(runningRef: MutableRefObject<boolean>, setGrid: (value: (
               }
             }
           }
-        })
+        });
       });
       _startTime = getCurrentTime();
     }
---
+++ src/constant.ts
@@ -0,0 +1,13 @@
+export const itemSize = 15;
+export const numRows = 50;
+export const numCols = 50;
+export const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
